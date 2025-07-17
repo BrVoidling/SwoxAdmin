6783379fd625b87b96460d9163540c207608fee7
Sven van Rijn <vanrijn@cardsolutions.nl>
2025-07-17 14:57:41 +0200

    migration moels

--- database/migrations/2025_07_14_142309_create_base_tables.php
+++ database/migrations/2025_07_14_142309_create_base_tables.php
@@ -13,22 +13,49 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('completed')->default(false);
+            $table->boolean('is_repeat_task')->default(false);
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
 
         Schema::create('repeat_tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('user_id');
+            $table->text('description')->nullable();
+            $table->string('frequency');
+            $table->timestamp('next_run');
+            $table->timestamp('last_run')->nullable();
             $table->timestamps();
         });
 
-        Schema::create('lists', function (Blueprint $table) {
+        Schema::create('tracking_lists', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
 
         Schema::create('list_items', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('list_id');
+            $table->text('json_data')->nullable();
             $table->timestamps();
+
+            $table->foreign('list_id')->references('id')->on('lists')->onDelete('cascade');
         });
     }
 
