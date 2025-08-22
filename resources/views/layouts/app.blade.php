e18f5cdd32b857806d436a9781b5900b0b5542c4
Sven van Rijn <vanrijn@cardsolutions.nl>
2025-08-22 16:28:57 +0200

    layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,8 @@
             </style>
         @endif
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
+    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex items-center lg:justify-center min-h-screen flex-col">
+        <header class="w-full bg-blue-500 text-sm mb-6 p-2 ">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
                     @auth
@@ -56,5 +56,10 @@
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
+        <footer class="w-full lg:max-w-4xl max-w-[335px] text-sm mt-6 items-center">
+            <p class="text-[#1b1b18] dark:text-[#EDEDEC] text-center">
+                &copy; {{ date('Y') }} Laravel. All rights reserved.
+            </p>
+        </footer>
     </body>
 </html>
